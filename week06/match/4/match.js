83a765033b78ab5076cfc46c56a159ae7a48783e
Tim-ao <1194522057@qq.com>
2020-05-19 16:18:27 +0800

    Create match.js

---
+++ week06/match/4/match.js
@@ -0,0 +1,66 @@
+/**
+ * 使用 mealy（米粒）型状态机从一个字符串中找出 abcdef
+ */
+
+function match(string) {
+  let state = start;
+  for (let c of string) {
+    state = state(c);
+  }
+  return state === end;
+}
+
+function start(c) {
+  if (c === 'a') {
+    return foundA
+  } else {
+    return start
+  }
+}
+
+function foundA(c) {
+  if (c === "b") {
+    return foundB;
+  } else {
+    return start(c)
+  }
+}
+
+function foundB(c) {
+  if (c === 'c') {
+    return foundC;
+  } else {
+    return start(c)
+  }
+}
+
+function foundC(c) {
+  if (c === 'd') {
+    return foundD;
+  } else {
+    return start(c)
+  }
+}
+
+function foundD(c) {
+  if (c === 'e') {
+    return foundE
+  } else {
+    return start(c)
+  }
+}
+
+function foundE(c) {
+  if (c === 'f') {
+    return end
+  } else {
+    return start(c)
+  }
+}
+
+
+function end() {
+  return end;
+}
+
+console.log(match('i aabcdef great'));
