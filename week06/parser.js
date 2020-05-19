87d39e0fa4c3172ce89ef6a85efee36dcf07b812
Tim-ao <1194522057@qq.com>
2020-05-19 17:45:49 +0800

    Create parser.js

---
+++ week06/parser.js
@@ -0,0 +1,78 @@
+const EOF = Symbol("EOF"); // End Of File
+
+function data(c) {
+  if (c === "<") {
+    return tagOpen;
+  } else if (c === EOF) {
+    return;
+  } else {
+    return data;
+  }
+}
+
+function tagOpen(c) {
+  if (c === '/') {
+    return endTagOpen;
+  } else if (c.match(/^[a-zA-Z]$/)) {
+    return tagName;
+  } else {
+    return;
+  }
+}
+
+function endTagOpen(c) {
+  if (c.match(/^[a-zA-Z]$/)) {
+    currentToken = {
+      type: "endTag",
+      tagName: ""
+    };
+    return tagName;
+  } else {
+    return;
+  }
+}
+
+function tagName(c) {
+  if (c.match(/^[\t\n\f]$/)) {
+    return beforeAttributeName;
+  } else if (c === "/") {
+    return selfCloseStartTag;
+  } else if (c.match(/^[a-zA-Z]$/)) {
+    return tagName(c);
+  } else if (c === '>') {
+    return data;
+  } else {
+    return tagName
+  }
+}
+
+function beforeAttributeName(c) {
+  if (c.match(/^[\t\n\f]$/)) {
+     return beforeAttributeName;
+  } else if (c === ">") {
+     return data;
+  } else if (c === "=") {
+    return beforeAttributeName;
+  } else {
+    return beforeAttributeName
+  }
+}
+
+function selfCloseStartTag(c) {
+   if (c === '>') {
+     currentToken.isSelfClose = true;
+     return data
+   } else if (c === EOF) {
+
+   } else {
+
+   }
+}
+
+module.exports.parseHTML = function (html) {
+  let state = data;
+  for (let c of html) {
+    state = state(c);
+  }
+  return state === EOF;
+};
