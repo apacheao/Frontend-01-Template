109ce366458ac4874682633a4d1e3f76405e47c3
Tim-ao <1194522057@qq.com>
2020-05-29 12:09:58 +0800

    Update render.js

--- week07/Toy Browser/render.js
+++ week07/Toy Browser/render.js
@@ -1,16 +1,22 @@
 const images = require("images");
 
 function render(viewport, element) {
-    if (element.style) {
-        var img = images(element.style.width, element.style.height);
-
-        if (element.style["background-color"]) {
-            let color = element.style["background-color"] || "rgb(0,0,0)";
-            color.match(/rgb\((\d+),(\d+),(\d+),(\d+)\)/);
-            img.fill(Number(RegExp.$1), Number(RegExp.$2), Number(RegExp.$3), 1);
-            viewport.draw(img, element.style.left || 0, element.style.top || 0);
-        }
+  if (element.computedStyle && JSON.stringify(element.computedStyle) !== "{}") {
+    var img = images(parseInt(element.computedStyle.width.value.split('px')[0]), parseInt(element.computedStyle.height.value.split('px')[0]));
+    if (element.computedStyle["background-color"].value) {
+        let color = element.computedStyle["background-color"].value;
+        console.log(color)
+        color.match(/rgb\((\d+),(\d+),(\d+)\)/);
+        img.fill(Number(RegExp.$1), Number(RegExp.$2), Number(RegExp.$3), 1);
+        viewport.draw(img, 20, 30);
     }
+  }
+
+  if (element.children) {
+    for (var child of element.children) {
+      render(viewport, child)
+    }
+  }
 }
 
 module.exports = render;
